300470b4b2ed359ec133a0d9775f066d96bbe4b2
bens0101 <156142599+bens0101@users.noreply.github.com>
2025-07-05 18:09:28 -0400

    testing pls work

--- src/app/(protected)/(tabs)/dashboard/index.tsx
+++ src/app/(protected)/(tabs)/dashboard/index.tsx
@@ -136,16 +136,3 @@ const Dashboard = () => {
 
 export default Dashboard;
 
-import { View, Text } from 'react-native';
-import React from 'react';
-import { tabContentStyles } from "../../../../theme/styles";
-
-export default function Dashboard() {
-    return (
-        <View style={tabContentStyles.container}>
-            <Text style={tabContentStyles.title}>Dashboard</Text>
-            <Text style={tabContentStyles.subtitle}>Welcome to your dashboard</Text>
-        </View>
-    );
-};
-
